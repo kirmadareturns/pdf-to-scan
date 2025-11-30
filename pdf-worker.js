94407ec1d84f500b4839255e6dec00ff739de05d
kirmadareturns <kirmadareturns@gmail.com>
2025-11-30 17:28:20 +0530

    Update pdf-worker.js

--- pdf-worker.js
+++ pdf-worker.js
@@ -1,17 +1,20 @@
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js');
 
-self.onmessage = async function (e) {
+self.onmessage = async (e) => {
     const { arrayBuffer, pageIndex } = e.data;
-
     const pdf = await pdfjsLib.getDocument(arrayBuffer).promise;
     const page = await pdf.getPage(pageIndex);
 
-    const viewport = page.getViewport({ scale: 1.0 });
-    const offscreen = new OffscreenCanvas(viewport.width, viewport.height);
-    const ctx = offscreen.getContext('2d', { alpha: false });
+    const viewport = page.getViewport({ scale: 2.0 });
+    const canvas = new OffscreenCanvas(viewport.width, viewport.height);
+    const ctx = canvas.getContext('2d', { alpha: false });
 
     await page.render({ canvasContext: ctx, viewport }).promise;
+    const blob = await canvas.convertToBlob({ type: 'image/jpeg', quality: 0.7 });
 
-    const blob = await offscreen.convertToBlob({ type: 'image/jpeg', quality: 0.9 });
-    self.postMessage({ blob, pageIndex });
+    const reader = new FileReader();
+    reader.onloadend = () => {
+        self.postMessage({ dataURL: reader.result, pageIndex });
+    };
+    reader.readAsDataURL(blob);
 };

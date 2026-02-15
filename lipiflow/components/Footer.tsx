51426e096eddaa304771b8afb7d7d450c300fe08
kirmadareturns <kirmadareturns@gmail.com>
2026-02-15 11:25:57 +0530

    Delete lipiflow directory

--- lipiflow/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer: React.FC = () => {
-  return (
-    <footer className="w-full py-6 text-center text-slate-500 text-sm">
-      <p>© {new Date().getFullYear()} LipiFlow. Powered by Gemini.</p>
-    </footer>
-  );
-};
